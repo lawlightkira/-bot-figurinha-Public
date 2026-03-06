c85ee6a2e4f33af15ebb3dd7cb3aa2f61ae735b4
lawlightkira <fernandamaciel879@gmail.com>
2026-03-06 20:42:47 -0300

    Initialize WhatsApp bot with baileys library

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
 
+async function startBot() {
+
+const { state, saveCreds } = await useMultiFileAuthState("auth")
+
+const sock = makeWASocket({
+auth: state
+})
+
+sock.ev.on("creds.update", saveCreds)
+
+console.log("Bot conectado ao WhatsApp")
+
+}
+
+startBot()
