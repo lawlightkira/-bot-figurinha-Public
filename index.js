3f6310bd961764608d74ded87cdcffe31817dfaf
lawlightkira <fernandamaciel879@gmail.com>
2026-03-07 13:19:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("@whiskeysockets/baileys")
+const express = require("express")
+const app = express()
+
+app.get("/", (req,res)=>{
+res.send("bot online")
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+console.log("server running")
+})const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("@whiskeysockets/baileys")
 
 async function startBot() {
 
