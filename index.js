a2a5e16eca6e6c6d3bb47469ef90a4d535ebc41e
lawlightkira <fernandamaciel879@gmail.com>
2026-03-07 12:02:01 -0300

    Enhance connection handling in WhatsApp bot
    Added DisconnectReason to handle connection updates.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require("@whiskeysockets/baileys")
 
 async function startBot() {
 
@@ -10,7 +10,20 @@ auth: state
 
 sock.ev.on("creds.update", saveCreds)
 
+sock.ev.on("connection.update", (update) => {
+
+const { connection } = update
+
+if(connection === "open") {
 console.log("Bot conectado ao WhatsApp")
+}
+
+if(connection === "close") {
+console.log("Reconectando...")
+startBot()
+}
+
+})
 
 }
 
